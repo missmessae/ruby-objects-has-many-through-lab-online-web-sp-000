763c43558e51facde73a8034a36daa7515f7a855
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 23:35:27 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,14 @@
 class Appointment
   @@all = []
 
+  def initialize(date, patient, doctor)
+    
   
+  The Appointment class needs a class method .all that lists each Appointment in the class variable.
 
+  An Appointment should be initialized with a date 
+  (as a string, like "Monday, August 1st"), a patient, and a doctor in this order. 
+  The Appointment should be saved in the @@all array.
 
 
 end
